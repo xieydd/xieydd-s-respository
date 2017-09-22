12bc69cdfbfc32ee68d77ec449a0dc9190803633
xieydd <xieydd@gmail.com>
2017-09-22 14:16:29 +0800

    时间客户端

--- xieydd_netty/src/main/java/Client/TimeClient.java
+++ xieydd_netty/src/main/java/Client/TimeClient.java
@@ -1,5 +1,17 @@
 package Client;
+/**
+ * 
 
+* @ClassName: TimeClient 
+
+* @Description: TODO(时间客户端) 
+
+* @author xieydd xieydd@gmail.com  
+
+* @date 2017-9-22 下午2:15:12 
+
+*
+ */
 public class TimeClient {
-
+	
 }
